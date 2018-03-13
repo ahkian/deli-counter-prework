7a15b79aec9a1c0d94099d8cff95c9c936627c88
Daniel Brazel <dbrazel4@gmail.com>
2018-03-13 16:21:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,12 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    return puts "There is no one in line."
+  else
+    puts "Now serving #{katz_deli[0]}"
+    katz_deli.pop 
+  end
+end
