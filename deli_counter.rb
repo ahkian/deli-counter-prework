26c9ad3da34258df165a0376589bf838b457625d
Daniel Brazel <dbrazel4@gmail.com>
2018-03-13 16:05:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli =[]
+
+def line
+  if katz_deli.length == 0
+    return "The line is currently empty."
+  #else
+    
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  "#{name} you are number #{katz_deli.length}."
+end
+  
